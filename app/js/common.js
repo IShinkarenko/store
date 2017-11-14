47ddcb25b6edc9f72fa32c8c8ef747036e2fc29b
IShinkarenko <ivan.shunkarenko@gmail.com>
2017-11-14 02:37:44 +0200

    add header

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,26 @@
 $(function() {
 
-	// Custom JS
+ 'use strict';
+
+	$('ul.sf-menu').superfish({
+		delay: 200, 
+		cssArrows: false, 
+		onInit : function() {
+			$(this).find('ul').css('display','none');
+		}
+	});
+
+	var $hamburger = $(".hamburger");
+  $hamburger.on("click", function() {
+    $hamburger.toggleClass("is-active");
+  });
+
+  $('.home-banner__carousel').owlCarousel({
+  	items: 1,
+    loop: true,
+    dots: true,
+	})
+
 
 });
+	
